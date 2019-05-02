c32259236e28dfc245951cb7fb510570c604f71c
Geo Ste <stein.geoffrey@gmail.com>
2019-05-02 02:03:01 +0200

    README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# export_bdd
+# export_bdd.sh
+
+Simply export a single MySQL database from your local env to an unique named file.
+Should greatly work within a cron job.
+
+Before the first execution, remember to make it executable by typing :
+`chmod +x export_single_bdd.sh`
+
+Then :
+`./export_single_bdd.sh`
+
--- export_single_bdd.sh
+++ export_single_bdd.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-USER="root"
-read -s -p "> Root password:" PASSWORD
+# Credentials
+read -p "> User:" USER
+read -s -p "> Password:" PASSWORD
 
+# Fetch DB list
 databases=`mysql -u $USER -p$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
 
+# DB choices
 echo -e '\n> DB Found :'
-
 select DB_EXPORT in $databases;
 do
 	echo '> DB to export :' $DB_EXPORT \($REPLY\)
 	break;
 done
 
+# Export
 echo 'start'
-mysqldump -u $USER -p$PASSWORD --databases $DB_EXPORT > ~/dumps/`date +%Y%m%d`.$DB_EXPORT.sql
+mysqldump -u $USER -p$PASSWORD --databases $DB_EXPORT > ./`date +%Y%m%d`-`date +%H%M%S`.$DB_EXPORT.sql
 echo 'done'
